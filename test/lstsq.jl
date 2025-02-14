dd5f4e8dd446ffba5b535832a8916c06561b501f
GiggleLiu <cacate0129@gmail.com>
2025-02-14 19:37:30 +0800

    update

--- test/lstsq.jl
+++ test/lstsq.jl
@@ -1,6 +1,7 @@
 using BackwardsLinalg
 using Test, Random
 using Zygote
+import Mooncake, DifferentiationInterface
 
 function gradient_check(f, args...; η = 1e-5)
     g = gradient(f, args...)
@@ -28,4 +29,26 @@ end
     tfuncb(b) = tfunc(A, b)
     @test gradient_check(tfuncA, A)
     @test gradient_check(tfuncb, b)
+end
+
+@testset "mooncake" begin
+    T = Float64
+    Random.seed!(3)
+    M, N = 10, 5
+    A = randn(T, M, N)
+    b = randn(T, M)
+    op = randn(N, N)
+    op += op'
+
+    function tfunc(A, b)
+        x = BackwardsLinalg.lstsq(A, b)
+        return x'*op*x
+    end
+    g1 = Zygote.gradient(tfunc, A, b)
+    backend = DifferentiationInterface.AutoMooncake(; config=nothing)
+    wrapped(x) = tfunc(x...)
+    Mooncake.@from_rrule Mooncake.DefaultCtx Tuple{typeof(BackwardsLinalg.lstsq), Matrix{Float64}, Vector{Float64}}
+    prep = DifferentiationInterface.prepare_gradient(wrapped, backend, (A, b))
+    g2 = DifferentiationInterface.gradient(wrapped, prep, backend, (A, b))
+    @test all(g1 .≈ g2)
 end
